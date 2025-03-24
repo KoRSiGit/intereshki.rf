e024d4466969eae707c735c767c7ed3d916d315d
KoRSGit <korsik@mail.ru>
2025-03-25 00:53:40 +0700

    Seven class

---
+++ _app/immutable/nodes/45.3f49c11e.js
@@ -0,0 +1,139 @@
+import{y as P,e as V}from"../chunks/Resume.svelte_svelte_type_style_lang.3eaad147.js";import{S as j,i as I,s as S,k as f,a as w,y as T,l as m,m as g,h as a,c as x,z as q,n as i,b as $,G as u,A as z,M as D,g as C,d as A,B as M,o as Q,H as N}from"../chunks/index.318290c2.js";/* empty css                                                     */import{T as B}from"../chunks/Title.bc78bbe7.js";import{q as G}from"../chunks/quizdown.0fe2a711.js";/* empty css                                                            */import{t as k}from"../chunks/meta.5675ef3f.js";async function H(){return{yearsOfExperience:P,experienceEntries:V}}const en=Object.freeze(Object.defineProperty({__proto__:null,load:H},Symbol.toStringTag,{value:"Module"})),L=`---\r
+primaryColor: orange\r
+secondaryColor: '#e8e8e8'\r
+textColor: black\r
+shuffleQuestions: false\r
+shuffleAnswers: True\r
+locale: ru\r
+---\r
+\r
+# Неравнодушные люди\r
+\r
+Назовите российскую общественную организацию, которая\r
+объединяет людей, неравнодушных к природе Родины, стремящихся к\r
+географическому познанию России и мира во всем их многообразии?\r
+\r
+> Основанно в 1845 году.\r
+\r
+- [ ] Всемирное общество защиты животных\r
+- [ ] Общество защиты интернета\r
+- [ ] Ассоция защиты от миног\r
+- [x] Русское географическое общество\r
+\r
+# Парусное судно В. Беринга?\r
+\r
+> Военно-морской Андреевский флаг ![](https://regnum.ru/uploads/pictures/news/2016/06/30/regnum_picture_1467276067342204_normal.jpg)\r
+\r
+- [x] Пакетбот "Святой Пётр"<br> ![](https://miniflot.ru/plans/XVIII/SvPetr/SvPetr3.jpg)\r
+- [ ] Парусно-моторная шхуна "Фрам"<br> ![](http://boar.home.pl/aaorlinskicom/wp-content/uploads/2013/04/P7250250a.jpg)\r
+- [ ] Корабль поморов - Коч<br> ![](https://img6.eadaily.com/o/53d/079fb435f03bdefe610c8b20b8d7e.jpeg)\r
+\r
+# Кто открыл Берингов пролив?\r
+\r
+> Изначально пролив открыт в конце 17 века.\r
+\r
+- [ ] Витус Беринг\r
+- [ ] Семён Челюскин\r
+- [ ] Ермак\r
+- [ ] Василий Поярков\r
+- [x] Семён Дежнёв\r
+\r
+# Новая Гвинея\r
+\r
+Как назывался военный корвет, на котором Н.Миклухо-Маклай отправился в свое первое путешествие в Новую Гвинею?\r
+\r
+![](https://pokypki.net/wp-content/uploads/2021/12/5f62b11dd1.png)\r
+\r
+- [ ] Изумруд\r
+- [ ] Вестник\r
+- [x] Витязь\r
+\r
+# Антарктида\r
+\r
+Антарктида была открыта русской экспедицией под руководством Фаддея Беллинсгаузена и Михаила Лазарева, которые на шлюпах "Восток" и "Мирный" подошли к шельфовому леднику. В каком году это произошло?\r
+\r
+![](https://pokypki.net/wp-content/uploads/2021/12/ddc371e587.png)\r
+\r
+- [ ] 1827\r
+- [ ] 1818\r
+- [x] 1820\r
+\r
+# Кто руководил Первой и Второй Камчатскими экспедициями, прошёл по проливу между Чукоткой и Аляской, достиг Северной Америки и открыл ряд островов Алеутской гряды?\r
+\r
+![](https://pokypki.net/wp-content/uploads/2021/12/bering.jpg)\r
+\r
+- [ ] Семён Дежнев\r
+- [ ] Иван Крузенштерн\r
+- [x] Витус Беринг\r
+\r
+# Человек и пароход\r
+\r
+Иван Фёдорович Крузенштерн - русский мореплаватель, возглавлявший в 1803-1806 годах первое русское кругосветное плавание. \r
+Как назывался корабль, которым командовал Крузенштерн?\r
+\r
+![](https://pokypki.net/wp-content/uploads/2021/12/4f1211b3c7.png)\r
+\r
+- [ ] Мария\r
+- [ ] Секрет\r
+- [x] Надежда\r
+\r
+# Хождение за три моря\r
+\r
+Русский путешественник, писатель, тверской купец, автор знаменитых путевых записей, известных под названием "Хожение за три моря". \r
+Кто он?<br/>\r
+\r
+![](https://pokypki.net/wp-content/uploads/2021/12/scale_1200-8.jpg)\r
+\r
+- [ ] Михаил Тверской\r
+- [ ] Николай Семенов\r
+- [x] Афанасий Никитин\r
+\r
+# Петербургская Академия наук наградила русского путешественника, географа и натуралиста медалью с надписью: "Первому исследователю природы Центральной Азии". \r
+\r
+Как его звали?<br/>\r
+\r
+![](https://pokypki.net/wp-content/uploads/2021/12/68202f42fb.png)\r
+\r
+- [x] Николай Михайлович Пржевальский\r
+- [ ] Пётр Кузьмич Козлов\r
+- [ ] Борис Андреевич Вилькицкий\r
+\r
+# Семен Дежнев - русский путешественник, землепроходец, мореход, исследователь Северной, Восточной Сибири и Северной Америки. \r
+\r
+В каком году мыс Восточный был переименован в мыс Дежнёва?\r
+\r
+![](https://pokypki.net/wp-content/uploads/2021/12/6f69bcdb17.png)\r
+\r
+- [ ] 1889\r
+- [ ] 1875\r
+- [x] 1898\r
+\r
+# Михаил Петрович Лазарев - русский флотоводец, мореплаватель, адмирал, командующий Черноморским флотом и один из первооткрывателей Антарктиды. \r
+\r
+Он был одним из основателей города - какого?\r
+\r
+![](https://pokypki.net/wp-content/uploads/2021/12/4ff208fca0cd.jpg)\r
+\r
+- [x] Новороссийск\r
+- [ ] Николаев\r
+- [ ] Севастополь\r
+\r
+# Юрий Фёдорович Лисянский - русский мореплаватель, исследователь, писатель, военный. \r
+\r
+Наиболее известен как командир шлюпа во время первой российской кругосветной экспедиции. Как назывался корабль (шлюп)?\r
+\r
+![](https://pokypki.net/wp-content/uploads/2021/12/yurij_lisyanskij.jpg)\r
+\r
+- [x] Нева\r
+- [ ] Брячислав\r
+- [ ] Эммануил\r
+\r
+# Владимир Васильевич Атласов - русский землепроходец, якутский казак. \r
+\r
+Кто назвал назвал Атласова "Камчатским Ермаком"?\r
+\r
+![](https://pokypki.net/wp-content/uploads/2021/12/scale_1200-1-5.jpg)\r
+\r
+- [x] Александр Сергеевич Пушкин\r
+- [ ] Влади́мир Лео́нтьевич Комаро́в \r
+- [ ] Евгений Абрамович Баратынский`;function R(b){let n,r,e,c,s,p,o,t,l,h,v;return t=new B({}),{c(){n=f("article"),r=f("div"),e=f("div"),c=w(),s=f("div"),p=w(),o=f("section"),T(t.$$.fragment),l=w(),h=f("section"),this.h()},l(d){n=m(d,"ARTICLE",{class:!0});var _=g(n);r=m(_,"DIV",{class:!0});var y=g(r);e=m(y,"DIV",{class:!0}),g(e).forEach(a),c=x(y),s=m(y,"DIV",{class:!0}),g(s).forEach(a),y.forEach(a),p=x(_),o=m(_,"SECTION",{class:!0});var E=g(o);q(t.$$.fragment,E),E.forEach(a),l=x(_),h=m(_,"SECTION",{class:!0});var O=g(h);O.forEach(a),_.forEach(a),this.h()},h(){i(e,"class","blob one svelte-ntg84b"),i(s,"class","blob two svelte-ntg84b"),i(r,"class","blobs svelte-ntg84b"),i(o,"class","name-and-contact svelte-ntg84b"),i(h,"class","about quizdown svelte-ntg84b"),i(n,"class","resume svelte-ntg84b")},m(d,_){$(d,n,_),u(n,r),u(r,e),u(r,c),u(r,s),u(n,p),u(n,o),z(t,o,null),u(n,l),u(n,h),v=!0},p:D,i(d){v||(C(t.$$.fragment,d),v=!0)},o(d){A(t.$$.fragment,d),v=!1},d(d){d&&a(n),M(t)}}}function X(b){return Q(()=>{let n=document.querySelector(".quizdown"),r={locale:"en",shuffleAnswers:!1};G.createApp(L,n,r)}),[]}class F extends j{constructor(n){super(),I(this,n,X,R,S,{})}}function J(b){let n,r,e,c,s,p,o;return document.title=n="Викторина - "+k,p=new F({}),{c(){r=f("meta"),e=f("meta"),c=w(),s=f("div"),T(p.$$.fragment),this.h()},l(t){const l=N("svelte-1fqj0m9",document.head);r=m(l,"META",{property:!0,content:!0}),e=m(l,"META",{name:!0,content:!0}),l.forEach(a),c=x(t),s=m(t,"DIV",{class:!0});var h=g(s);q(p.$$.fragment,h),h.forEach(a),this.h()},h(){i(r,"property","og:title"),i(r,"content","Викторина - "+k),i(e,"name","twitter:title"),i(e,"content","Викторина - "+k),i(s,"class","resume-wrapper svelte-uau33l")},m(t,l){u(document.head,r),u(document.head,e),$(t,c,l),$(t,s,l),z(p,s,null),o=!0},p(t,[l]){(!o||l&0)&&n!==(n="Викторина - "+k)&&(document.title=n)},i(t){o||(C(p.$$.fragment,t),o=!0)},o(t){A(p.$$.fragment,t),o=!1},d(t){a(r),a(e),t&&a(c),t&&a(s),M(p)}}}function K(b,n,r){let{data:e}=n;return b.$$set=c=>{"data"in c&&r(0,e=c.data)},[e]}class sn extends j{constructor(n){super(),I(this,n,K,J,S,{data:0})}}export{sn as component,en as universal};
---
+++ _app/immutable/nodes/46.4b2dd4f8.js
@@ -0,0 +1,54 @@
+import{y as Q,e as G}from"../chunks/Resume.svelte_svelte_type_style_lang.3eaad147.js";import{S as z,i as A,s as q,k as h,q as T,a as w,l as m,m as p,r as I,h as o,c as y,n as d,b as S,G as u,M as x,y as M,z as j,A as k,g as D,d as O,B as V,o as L,H as N}from"../chunks/index.318290c2.js";/* empty css                                                     */import{q as P}from"../chunks/quizdown.0fe2a711.js";/* empty css                                                            */import{t as E}from"../chunks/meta.5675ef3f.js";async function B(){return{yearsOfExperience:Q,experienceEntries:G}}const ae=Object.freeze(Object.defineProperty({__proto__:null,load:B},Symbol.toStringTag,{value:"Module"}));function F(v){let e,n,s,l,r,c;return{c(){e=h("div"),n=h("h1"),s=T("Различия систем образования"),l=w(),r=h("h5"),c=T("Викторина по страноведению (Русская/Британская)"),this.h()},l(a){e=m(a,"DIV",{class:!0});var t=p(e);n=m(t,"H1",{class:!0});var i=p(n);s=I(i,"Различия систем образования"),i.forEach(o),l=y(t),r=m(t,"H5",{class:!0});var f=p(r);c=I(f,"Викторина по страноведению (Русская/Британская)"),f.forEach(o),t.forEach(o),this.h()},h(){d(n,"class","svelte-1htz11t"),d(r,"class","svelte-1htz11t"),d(e,"class","resume-title")},m(a,t){S(a,e,t),u(e,n),u(n,s),u(e,l),u(e,r),u(r,c)},p:x,i:x,o:x,d(a){a&&o(e)}}}class R extends z{constructor(e){super(),A(this,e,null,F,q,{})}}const U=`---\r
+primaryColor: orange\r
+secondaryColor: '#e8e8e8'\r
+textColor: black\r
+shuffleQuestions: false\r
+shuffleAnswers: True\r
+locale: ru\r
+---\r
+\r
+# Условия обучения\r
+\r
+Какая система образования обеспечивает более **гибкие** условия для получения знаний в школе?\r
+\r
+![](https://www.ace.my/wp-content/uploads/2021/10/Hybrid-Learning-04.png)\r
+\r
+> В этой системе небольшой список обязательных предметов.\r
+\r
+- [ ] Южно-Африканская\r
+- [ ] Российская\r
+- [x] Британская\r
+- [ ] Немецкая\r
+\r
+# Базовый уровень\r
+\r
+Является ли **музыка** обязательным предметом в Великобритании?\r
+\r
+> Класс в школе Великобритании ![](https://www.schoolhousemagazine.co.uk//wp-content/uploads/2017/01/Farleigh-Schools-for-Music.jpg)\r
+\r
+- [x] Является обязательной\r
+- [ ] Является необязательной\r
+- [ ] Мызыка не изучается\r
+\r
+# Начало\r
+\r
+Со скольки лет начинается учёба в Великобритании?\r
+\r
+![](https://britishschoolalmeria.com/wp-content/uploads/2019/06/ASC5864.jpg)\r
+\r
+> Обязательная подготовка - залог успешной работы.\r
+\r
+- [ ] 4 года\r
+- [x] 5 лет\r
+- [ ] 8 лет\r
+\r
+# General Education\r
+\r
+Until what **age** does general education last?\r
+\r
+> ![](https://static.independent.co.uk/2023/01/12/15/iStock-1300847281.jpg?quality=75&width=990&auto=webp&crop=982:726,smart)\r
+\r
+- [ ] Age of 14\r
+- [ ] Age of 21\r
+- [x] Age of 16 \r
+`;function J(v){let e,n,s,l,r,c,a,t,i,f,b;return t=new R({}),{c(){e=h("article"),n=h("div"),s=h("div"),l=w(),r=h("div"),c=w(),a=h("section"),M(t.$$.fragment),i=w(),f=h("section"),this.h()},l(_){e=m(_,"ARTICLE",{class:!0});var g=p(e);n=m(g,"DIV",{class:!0});var $=p(n);s=m($,"DIV",{class:!0}),p(s).forEach(o),l=y($),r=m($,"DIV",{class:!0}),p(r).forEach(o),$.forEach(o),c=y(g),a=m(g,"SECTION",{class:!0});var C=p(a);j(t.$$.fragment,C),C.forEach(o),i=y(g),f=m(g,"SECTION",{class:!0});var H=p(f);H.forEach(o),g.forEach(o),this.h()},h(){d(s,"class","blob one svelte-ntg84b"),d(r,"class","blob two svelte-ntg84b"),d(n,"class","blobs svelte-ntg84b"),d(a,"class","name-and-contact svelte-ntg84b"),d(f,"class","about quizdown svelte-ntg84b"),d(e,"class","resume svelte-ntg84b")},m(_,g){S(_,e,g),u(e,n),u(n,s),u(n,l),u(n,r),u(e,c),u(e,a),k(t,a,null),u(e,i),u(e,f),b=!0},p:x,i(_){b||(D(t.$$.fragment,_),b=!0)},o(_){O(t.$$.fragment,_),b=!1},d(_){_&&o(e),V(t)}}}function K(v){return L(()=>{let e=document.querySelector(".quizdown"),n={locale:"en",shuffleAnswers:!1};P.createApp(U,e,n)}),[]}class W extends z{constructor(e){super(),A(this,e,K,J,q,{})}}function X(v){let e,n,s,l,r,c,a;return document.title=e="Викторина - "+E,c=new W({}),{c(){n=h("meta"),s=h("meta"),l=w(),r=h("div"),M(c.$$.fragment),this.h()},l(t){const i=N("svelte-1fqj0m9",document.head);n=m(i,"META",{property:!0,content:!0}),s=m(i,"META",{name:!0,content:!0}),i.forEach(o),l=y(t),r=m(t,"DIV",{class:!0});var f=p(r);j(c.$$.fragment,f),f.forEach(o),this.h()},h(){d(n,"property","og:title"),d(n,"content","Викторина - "+E),d(s,"name","twitter:title"),d(s,"content","Викторина - "+E),d(r,"class","resume-wrapper svelte-uau33l")},m(t,i){u(document.head,n),u(document.head,s),S(t,l,i),S(t,r,i),k(c,r,null),a=!0},p(t,[i]){(!a||i&0)&&e!==(e="Викторина - "+E)&&(document.title=e)},i(t){a||(D(c.$$.fragment,t),a=!0)},o(t){O(c.$$.fragment,t),a=!1},d(t){o(n),o(s),t&&o(l),t&&o(r),V(c)}}}function Y(v,e,n){let{data:s}=e;return v.$$set=l=>{"data"in l&&n(0,s=l.data)},[s]}class oe extends z{constructor(e){super(),A(this,e,Y,X,q,{data:0})}}export{oe as component,ae as universal};
